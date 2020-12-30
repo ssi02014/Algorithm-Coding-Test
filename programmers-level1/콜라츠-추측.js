f5f819cf4874bfe458e4364c8c1c0eedb2d565aa
JeonMinJae <ssi02014@naver.com>
2020-12-31 06:00:58 +0900

    하샤드 수

--- programmers-level1/콜라츠-추측.js
+++ programmers-level1/콜라츠-추측.js
@@ -1,12 +1,12 @@
 function solution(num) {
     let count = 0;
     while(true){
-        if (num == 1) return count;
+        if (num === 1) return count;
         
-        if (num % 2 == 1) {
+        if (num % 2 === 1) {
             num = num * 3 + 1;
             count++;
-        } else if (num % 2 == 0) {
+        } else if (num % 2 === 0) {
             num = num / 2;
             count++;
         }
