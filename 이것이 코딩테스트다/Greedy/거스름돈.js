0a36023d1aeb544167315fcf323cf9198ae734d1
JeonMinJae <ssi02014@naver.com>
2021-01-23 17:04:38 +0900

    Implement: 1이될때까지

--- 이것이 코딩테스트다/Greedy/거스름돈.js
+++ 이것이 코딩테스트다/Greedy/거스름돈.js
@@ -1,12 +1,18 @@
 function solution(n) {
-    let money = [500, 100, 50, 10];
+    let moneys = [500, 100, 50, 10];
     let count = 0;
 
-    for(let i = 0; i < money.length; i++) {
-        count += Math.floor(n / money[i]) //2
-        n = n % money[i];
+    for(let money of moneys) {
+        count += Math.floor(n / money);
+        n = n % money;
     }
+
     return count;
+
+    // for(let i = 0; i < money.length; i++) {
+    //     count += Math.floor(n / money[i]) //2
+    //     n = n % money[i];
+    // }
 }
 
 console.log(solution(1260));
