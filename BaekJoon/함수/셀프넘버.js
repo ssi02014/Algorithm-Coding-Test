082237114bc68e172a1bcd991173ce10536be9cd
JeonMinJae <ssi02014@naver.com>
2021-02-02 04:17:11 +0900

    Refactor(백준): 셀프 넘버 리팩토링

--- BaekJoon/함수/셀프넘버.js
+++ BaekJoon/함수/셀프넘버.js
@@ -1,9 +1,8 @@
-let selfNumber = 0;
-let selfNumberList = [];
+const selfNumberList = [];
 
 function solution() {
     for(let i = 1; i <= 10000; i++) {
-        extractNumber(i);
+        extractSelfNumber(i);
     }   
 
     for(let i = 1; i <= 10000; i++) {
@@ -11,16 +10,14 @@ function solution() {
     }
 }
 
-function extractNumber(n) {
-    if(n < 10) {
-        selfNumber = 2 * n;
-    } else if(n < 100) {
-        selfNumber = n + parseInt(n / 10) + (n % 10);
-    } else if (n < 1000) {
-        selfNumber = n + parseInt(n / 100) + parseInt((n % 100) / 10) + (n % 10);
-    } else {
-        selfNumber = n + parseInt(n / 1000) + parseInt((n % 1000) / 100) + parseInt((n % 100) / 10) +(n % 10);
-    }
+function extractSelfNumber(n) {
+    let selfNumber = 0;
+
+    if(n < 10) selfNumber = 2 * n;
+    else if(n < 100) selfNumber = n + parseInt(n / 10) + (n % 10);
+    else if (n < 1000) selfNumber = n + parseInt(n / 100) + parseInt((n % 100) / 10) + (n % 10);
+    else selfNumber = n + parseInt(n / 1000) + parseInt((n % 1000) / 100) + parseInt((n % 100) / 10) +(n % 10);
+
     selfNumberList.push(selfNumber);
 }
 
