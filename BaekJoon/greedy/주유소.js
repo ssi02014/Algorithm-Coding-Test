2f5a43b75d1409d0e9573d9187db8a472214e6e9
JeonMinJae <ssi02014@naver.com>
2021-02-10 14:31:35 +0900

    Implement: 주유소

--- BaekJoon/greedy/주유소.js
+++ BaekJoon/greedy/주유소.js
@@ -10,15 +10,16 @@ rl.on("line", function (line) {
     input.push(line); 
 }).on("close", function () { 
     const cityCount = parseInt(input.shift());
-    const distance = input.shift().split(" ").map(Number);
-    const oilCost = input.shift().split(" ").map(Number);
+    const distance = input.shift().split(" ").map(el => BigInt(el));
+    const oilCost = input.shift().split(" ").map(el => BigInt(el));
 
-    let sum = 0;
+    let sum = BigInt(0);
     let minCost = oilCost[0];
 
     for (let i = 0; i < cityCount - 1; i++) {
         if (oilCost[i] < minCost) minCost = oilCost[i];
         sum += (minCost * distance[i]);
     }
-    console.log(sum);
+
+    console.log(`${sum}`);
 });
