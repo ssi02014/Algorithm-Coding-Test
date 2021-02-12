59e0fefb13ec4915173ba8174b2f6d395f9ab55a
JeonMinJae <ssi02014@naver.com>
2021-02-12 19:18:34 +0900

    Issue: post issue

--- BaekJoon/기본수학2/소인수분해.js
+++ BaekJoon/기본수학2/소인수분해.js
@@ -12,32 +12,26 @@ rl.on("line", function (line) {
     input.push(line); 
 }).on("close", function () { 
     let N = parseInt(input[0]);
-    let primeFactor = [];
     let count = 3;
-
-    if (N === 1) return; 
 
     while(N !== 1) {
         if (N % 2 === 0) {
             N = parseInt(N / 2);
-            primeFactor.push(2);
+            console.log(2);
         }
 
         if (N % 3 === 0) {
             N = parseInt(N / 3);
-            primeFactor.push(3);
+            console.log(3);
         }
 
         if (N % 2 !== 0 && N % 3 !== 0) {
             count++;
             if (N % count === 0) {
                 N = parseInt(N / count);
-                primeFactor.push(count);
+                console.log(count);
+                //post이슈
             }
         }
     }
-
-    let result = primeFactor.sort((a, b) => a - b);
-
-    console.log(result.join('\n'));
 });
