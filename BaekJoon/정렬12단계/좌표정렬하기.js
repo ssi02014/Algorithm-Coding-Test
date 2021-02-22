abf46ede39a2a6769802fcc914a50fabc2867416
JeonMinJae <ssi02014@naver.com>
2021-02-22 16:15:20 +0900

    Add(backjoon): 좌표정렬하기

--- BaekJoon/정렬12단계/좌표정렬하기.js
+++ BaekJoon/정렬12단계/좌표정렬하기.js
@@ -9,5 +9,19 @@ const input = [];
 rl.on("line", function (line) {
     input.push(line); 
 }).on("close", function () { 
-    
+    const N = parseInt(input.shift());
+
+    let location = [];
+    for(let i = 0; i < N; i++) {
+        location[i] = input[i].split(" ").map(Number);
+    }
+
+    let result = '';
+    location.sort((a, b) => {
+        return a[0] !== b[0] ? a[0] - b[0] : a[1] - b[1];
+    }).forEach(a => {
+        result += `${a[0]} ${a[1]}\n`;
+    });
+
+    console.log(result);
 });
