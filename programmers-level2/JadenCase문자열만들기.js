bf1a11e8fb8193a5a340f943f88e6b8d3aa97848
JeonMinJae <ssi02014@naver.com>
2021-02-24 21:30:32 +0900

    Add(Level2): JadenCase 문자열 만들기

--- programmers-level2/JadenCase문자열만들기.js
+++ programmers-level2/JadenCase문자열만들기.js
@@ -1,11 +1,25 @@
 function solution(s) {
-    const list = s.toLowerCase().split(" ");
+    const strList = s.toLowerCase().split(" ");
     
     let result = '';
     
-    result = list.map(str => {
+    result = strList.map(str => {
         return str === "" ? "" : str.replace(str[0], str[0].toUpperCase());
     }).join(" ");
     
     return result
 }
+
+/*
+    solution2 반복문
+
+    function solution(s) {
+        const list = s.toLowerCase().split(" ");
+        
+        for (let i = 0; i < list.length; i++) {
+            if (list[i] === "") list[i] = "";
+            else list[i] = list[i].replace(list[i][0], list[i][0].toUpperCase());
+        }
+        return list.join(" ");
+    }
+*/
--- programmers-level2/dist/JadenCase문자열만들기.dev.js
+++ programmers-level2/dist/JadenCase문자열만들기.dev.js
@@ -1,10 +1,23 @@
 "use strict";
 
 function solution(s) {
-  var list = s.toLowerCase().split(" ");
+  var strList = s.toLowerCase().split(" ");
   var result = '';
-  result = list.map(function (str) {
+  result = strList.map(function (str) {
     return str === "" ? "" : str.replace(str[0], str[0].toUpperCase());
   }).join(" ");
   return result;
 }
+/*
+    solution2 반복문
+
+    function solution(s) {
+        const list = s.toLowerCase().split(" ");
+        
+        for (let i = 0; i < list.length; i++) {
+            if (list[i] === "") list[i] = "";
+            else list[i] = list[i].replace(list[i][0], list[i][0].toUpperCase());
+        }
+        return list.join(" ");
+    }
+*/
