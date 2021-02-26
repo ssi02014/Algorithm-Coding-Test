22f5306c8fe5baf82066f0d2815924a1438e1827
JeonMinJae <ssi02014@naver.com>
2021-02-26 18:21:46 +0900

    Add(algorithm): 유클리드 호제법

---
+++ Algorithm/유클리드호제법.js
@@ -0,0 +1,17 @@
+//최대공약수 구하기
+
+//greatestCommonDivisior
+function gcd(a, b) {
+    return b ? gcd(b, a % b) : a;
+}
+
+
+//최소공배수 구하기
+
+//leastCommonMultiple
+function lcm(a, b) {
+    return a * b / gcd(a, b);
+}
+
+console.log(gcd(3, 6));
+console.log(lcm(12, 32));
