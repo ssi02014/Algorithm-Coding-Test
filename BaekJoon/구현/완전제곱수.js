be2813bef5b2073b79384cf2f2eb0faa8cffabb6
JeonMinJae <ssi02014@naver.com>
2021-02-26 17:24:51 +0900

    Add(baekjoon): 완전제곱수

---
+++ BaekJoon/구현/완전제곱수.js
@@ -0,0 +1,30 @@
+const readline = require("readline"); 
+const rl = readline.createInterface({ 
+    input: process.stdin, 
+    output: process.stdout, 
+}); 
+
+const input = [];
+
+rl.on("line", function (line) {
+    input.push(line); 
+}).on("close", function () { 
+    const N = parseInt(input[0]);
+    const M = parseInt(input[1]);
+    const result = [];
+    
+    let perfectSquareNum = 0;
+    
+    for(let i = 1; i * i <= M; i++) {
+        perfectSquareNum = i * i;
+        if (perfectSquareNum >= N && perfectSquareNum <= M) {
+            result.push(perfectSquareNum);
+        }
+    }
+
+    if (result.length === 0) console.log(-1);
+    else {
+        console.log(result.reduce((a, b) => a + b));
+        console.log(Math.min(...result));
+    }
+});
