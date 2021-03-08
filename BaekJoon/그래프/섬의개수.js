aebffeb1ef91835d3ab008d0b6d69d515be0042b
JeonMinJae <ssi02014@naver.com>
2021-03-08 23:07:49 +0900

    Add(baekjoon): 섬의 개수

--- BaekJoon/그래프/섬의개수.js
+++ BaekJoon/그래프/섬의개수.js
@@ -4,35 +4,78 @@ const rl = readline.createInterface({
   output: process.stdout, 
 }); 
 
-const input = [];
-const map = [];
-let [N, M] = [0, 0];
-let count = 0;
-let move_x = [-1, 1, 0, 0, -1, -1, 1, 1]; 
-let move_y = [0, 0,l -1, 1, -1, 1, -1, 1];
+//전역 변수
+const dx = [-1, 1, 0, 0, -1, -1, 1, 1]; 
+const dy = [0, 0, -1, 1, -1, 1, -1, 1];
 
-const rangeCheck = (x, y) => { 
-  if (x >= 0 && x < N && y >= 0 && y < M) { 
-    return true; 
-  } else return fase;
+let input = [];
+let map = [];
+let [w, h] = [0, 0];
+
+//solution
+const solution = () => {
+  let count = 0;
+
+  [w, h] = input.shift().split(" ").map(Number);
+
+  if (w === 0 & h === 0) return;
+  else if (w === 1 && h === 1) count = parseInt(input.shift());
+  else {
+    assignment(h, w);
+
+    for (let i = 0 ; i < h; i++) {
+      for (let j = 0; j < w; j++) {
+        if (map[i][j] === 1) {
+          DFS(i, j);
+          count++;
+        }
+      }
+    }
+  }
+
+  console.log(count);
 };
 
+//그래프에 위치 넣어주는 함수
+const assignment = (h, w) => {
+  map = Array.from(Array(h), () => Array(w).fill(0));
+
+  for(let i = 0; i < h; i++) {
+    let temp = input[i].split(" ").map(Number);
+
+    map[i] = temp;
+  }
+
+  input = input.slice(h);
+  console.log(input);
+};
+
+//DFS
+const DFS = (i, j) => {
+  if (rangeCheck(i, j) && map[i][j] === 1) {
+    map[i][j] = 0;
+
+    for (let k = 0; k < dx.length; k++) {
+      DFS(i + dx[k], j + dy[k]);
+    }
+  }
+};
+
+//배열 범위 체크
+const rangeCheck = (i, j) => { 
+  if (i >= 0 && i < h && j >= 0 && j < w) return true; 
+  else return false;
+};
 
 
 rl.on("line", function (line) {
   input.push(line); 
-}).on("close", function () { 
-  for(let i = 0; i < input.length; i++) {
-      [N, M] = input.shift().split(" ").map(Number);
+}).on("close", function () {
 
-      for(let j = 0; j < M; j++) {
-          map[j] = input.shift().split(" ").map(Number);
-      }
-      for(let j = 0 ; j < N; j++) {
-          for(let k =0; k < M; k++) {
-            if (findBlock(i, j))  count++;
-          }
-      }
+  while (input.length !== 0) {
+    solution();
+
+    //초기화
+    map = [];
   }
-  console.log(count);
 });
