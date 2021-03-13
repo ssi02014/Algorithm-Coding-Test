81ad559187b90e767cef9b4ecdd5a9d73c31a1f4
JeonMinJae <ssi02014@naver.com>
2021-03-13 14:27:55 +0900

    Add(codeup): 연월일 입력받은 값 그대로 출력하기

---
+++ CodeUp/입력값.js
@@ -0,0 +1,13 @@
+const readline = require("readline"); 
+const rl = readline.createInterface({ 
+  input: process.stdin, 
+  output: process.stdout, 
+}); 
+
+const input = [];
+
+rl.on("line", function (line) {
+  input.push(line); 
+}).on("close", function () { 
+  
+});
