90ef035905a95ece03e09f3653bdb26dea64af41
JeonMinJae <ssi02014@naver.com>
2021-04-21 13:22:07 +0900

    Add(minjae/baekjoon): 나머지

---
+++ BaekJoon/기본수학2/나머지.js
@@ -0,0 +1,18 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const input = [];
+
+rl.on("line", function (line) {
+  input.push(line);
+}).on("close", function () {
+  const [A, B, C] = input[0].split(" ").map(Number);
+
+  console.log((A + B) % C);
+  console.log(((A % C) + (B % C)) % C);
+  console.log((A * B) % C);
+  console.log(((A % C) * (B % C)) % C);
+});
