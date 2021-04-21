39b3877b35d3dceee6f8e49353c53d8b0f9cdcf7
JeonMinJae <ssi02014@naver.com>
2021-04-21 21:54:03 +0900

    Add(minjae/codeUp): 함께푸는날

--- CodeUp/기초100제/2차원배열/바둑판에흰돌놓기.js
+++ CodeUp/기초100제/2차원배열/바둑판에흰돌놓기.js
@@ -12,7 +12,7 @@ rl.on("line", function (line) {
   const N = parseInt(input.shift());
 
   let [a, b] = [0, 0];
-  let board = Array.from(Array(19), () => new Array(19).fill(0));
+  let board = Array.from(new Array(19), () => new Array(19).fill(0));
 
   for (let i = 0; i < N; i++) {
     [a, b] = input[i].split(" ").map(Number);
