cf1243c03e7a12ea3e630a46d4ff12cf5f7c60ee
JeonMinJae <ssi02014@naver.com>
2021-04-28 16:38:56 +0900

    Add(minjae/level2): 오픈채팅방(카카오)

---
+++ programmers-level2/오픈채팅방(카카오).js
@@ -0,0 +1,30 @@
+function solution(records) {
+  const result = [];
+  const temp = {};
+
+  let elements = [];
+
+  records.map((record) => {
+    elements = record.split(" ");
+
+    for (let i = 0; i < elements.length; i++) {
+      if (elements[0] === "Enter") {
+        temp[elements[1]] = elements[2];
+      } else if (elements[0] === "Change") {
+        temp[elements[1]] = elements[2];
+      }
+    }
+  });
+
+  records.map((record) => {
+    elements = record.split(" ");
+
+    if (elements[0] === "Enter") {
+      result.push(`${temp[elements[1]]}님이 들어왔습니다.`);
+    } else if (elements[0] === "Leave") {
+      result.push(`${temp[elements[1]]}님이 나갔습니다.`);
+    }
+  });
+
+  return result;
+}
