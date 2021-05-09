f4e37808baa6f12764afb5d591387feac3bd5e80
JeonMinJae <ssi02014@naver.com>
2021-05-09 19:09:38 +0900

    Add(minjae/baekjoon): 프린터 큐

--- BaekJoon/큐/프린터큐.js
+++ BaekJoon/큐/프린터큐.js
@@ -9,8 +9,37 @@ const input = [];
 rl.on("line", function (line) {
   input.push(line);
 }).on("close", function () {
-  let N = parseInt(input.shift());
+  const testCase = parseInt(input.shift());
 
-  console.log(N);
-  // for (let i = 0; i < N; i++) {}
+  for (let i = 0; i < testCase * 2; i += 2) {
+    const [N, M] = input[i].split(" ").map(Number);
+    const importance = input[i + 1].split(" ").map(Number);
+    const queue = [];
+
+    let count = 0;
+    let result = 0;
+
+    for (let j = 0; j < N; j++) {
+      queue.push(j);
+    }
+
+    while (queue.length !== 0) {
+      const maxNum = Math.max(...importance);
+
+      if (importance[0] < maxNum) {
+        importance.push(importance.shift());
+        queue.push(queue.shift());
+      } else {
+        count++;
+
+        importance.shift();
+        result = queue.shift();
+
+        if (result === M) {
+          console.log(count);
+          break;
+        }
+      }
+    }
+  }
 });
