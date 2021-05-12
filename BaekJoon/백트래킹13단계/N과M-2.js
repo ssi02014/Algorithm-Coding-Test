21b9b1f1569260e3bfa75d42c017a39de2de00fa
JeonMinJae <ssi02014@naver.com>
2021-05-12 14:56:25 +0900

    Add(minjae/baekjoon): N과 M (1)

--- BaekJoon/백트래킹13단계/N과M-2.js
+++ BaekJoon/백트래킹13단계/N과M-2.js
@@ -1,37 +1,37 @@
-const readline = require("readline"); 
-const rl = readline.createInterface({ 
-    input: process.stdin, 
-    output: process.stdout, 
-}); 
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 const input = [];
 
 rl.on("line", function (line) {
-    input.push(line); 
-}).on("close", function () { 
-    const [N, M] = input[0].split(" ").map(Number);
-    const visited = new Array(N);
-    const output = [];
+  input.push(line);
+}).on("close", function () {
+  const [N, M] = input[0].split(" ").map(Number);
+  const visited = new Array(N);
+  const output = [];
 
-    let result = '';
-    function recurison(idx, start) {
-        if (start === M) {
-            result += `${output.join(' ')}\n`;
-            return;
-        }
-
-        for (let i = idx; i < N; i++) {
-            if (visited[i]) continue;
-
-            visited[i] = true;
-            output.push(i + 1);
-            recurison(i, start + 1);
-            output.pop();
-            visited[i] = false;
-        }
+  let result = "";
+  function recurison(idx, start) {
+    if (start === M) {
+      result += `${output.join(" ")}\n`;
+      return;
     }
 
-    recurison(0, 0);
-    
-    console.log(result);
+    for (let i = idx; i < N; i++) {
+      if (visited[i]) continue;
+
+      visited[i] = true;
+      output.push(i + 1);
+      recurison(i, start + 1);
+      output.pop();
+      visited[i] = false;
+    }
+  }
+
+  recurison(0, 0);
+
+  console.log(result);
 });
