6a197c075e6fae77ec6b6a419287bd30bb0bbf25
JeonMinJae <ssi02014@naver.com>
2021-05-14 02:15:38 +0900

    Add(minjae/baekjoon): 에라토스테네스의 체

---
+++ BaekJoon/수학/에라토스테네스의체.js
@@ -0,0 +1,39 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const input = [];
+
+rl.on("line", function (line) {
+  input.push(line);
+}).on("close", function () {
+  const [N, K] = input[0].split(" ").map(Number);
+  const seq = [];
+  const result = [];
+
+  for (let i = 0; i <= N; i++) {
+    seq.push(true);
+  }
+
+  for (let i = 2; i * i <= N; i++) {
+    if (seq[i]) {
+      result.push(i);
+      seq[i] = false;
+
+      for (let j = i * i; j <= N; j += i) {
+        if (!result.includes(j)) {
+          result.push(j);
+          seq[j] = false;
+        }
+      }
+    }
+  }
+
+  seq.map((_, i) => {
+    if (!result.includes(i) && i > 1) result.push(i);
+  });
+
+  console.log(result[K - 1]);
+});
