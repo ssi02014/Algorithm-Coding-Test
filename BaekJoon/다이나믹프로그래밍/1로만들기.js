0ea6930caa64a165fa5118d5b189e2e4484b6b25
JeonMinJae <ssi02014@naver.com>
2021-05-29 20:13:11 +0900

    Add(minjae/baekjoon): 1로 만들기

--- BaekJoon/다이나믹프로그래밍/1로만들기.js
+++ BaekJoon/다이나믹프로그래밍/1로만들기.js
@@ -9,21 +9,19 @@ const input = [];
 rl.on("line", function (line) {
   input.push(line);
 }).on("close", function () {
-  let N = parseInt(input[0]);
-  let count = 1;
-  let list = Array.from(Array(N).fill(0));
+  const N = parseInt(input[0]);
 
-  for (let i = 2; i < N + 1; i++) {
+  let list = new Array(N + 1).fill(0);
+
+  for (let i = 2; i <= N; i++) {
     list[i] = list[i - 1] + 1;
+
     if (i % 2 === 0) {
       list[i] = Math.min(list[i], list[i / 2] + 1);
     }
     if (i % 3 === 0) {
       list[i] = Math.min(list[i], list[i / 3] + 1);
     }
-    if (i % 5 === 0) {
-      list[i] = Math.min(list[i], list[i / 5] + 1);
-    }
   }
-  console.log(list[N]);
+  console.log(list);
 });
