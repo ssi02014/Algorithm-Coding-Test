655c57335a9bb1161dc167298c295671f73f3fc7
JeonMinJae <ssi02014@naver.com>
2021-05-30 20:40:44 +0900

    Add(minjae/baekjoon): 경로 찾기

---
+++ BaekJoon/그래프/경로찾기.js
@@ -0,0 +1,35 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const input = [];
+
+rl.on("line", function (line) {
+  input.push(line);
+}).on("close", function () {
+  const N = parseInt(input[0]);
+  const graph = [];
+
+  let result = "";
+
+  for (let i = 1; i <= N; i++) {
+    graph[i - 1] = input[i].split(" ").map(Number);
+  }
+
+  for (let k = 0; k < N; k++) {
+    for (let i = 0; i < N; i++) {
+      for (let j = 0; j < N; j++) {
+        if (graph[i][j] == 1) continue;
+        if (graph[i][k] == 1 && graph[k][j] == 1) graph[i][j] = 1;
+      }
+    }
+  }
+
+  graph.map((el) => {
+    result += el.join(" ") + "\n";
+  });
+
+  console.log(result);
+});
