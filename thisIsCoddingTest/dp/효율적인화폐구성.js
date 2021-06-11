5124e255a97fc1ff0201981d303c1842954b81d3
JeonMinJae <ssi02014@naver.com>
2021-06-11 16:39:57 +0900

    Add(minjae/dp): 효율적인 화페구성

---
+++ thisIsCoddingTest/dp/효율적인화폐구성.js
@@ -0,0 +1,31 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const input = [];
+
+rl.on("line", function (line) {
+  input.push(line);
+}).on("close", function () {
+  const [N, M] = input[0].split(" ").map(Number);
+  const money = [];
+  const d = new Array(M + 1).fill(10001);
+
+  for (let i = 1; i <= N; i++) {
+    money[i - 1] = parseInt(input[i]);
+  }
+
+  d[0] = 0;
+
+  for (let i = 0; i < N; i++) {
+    for (let j = money[i]; j < M + 1; j += money[i]) {
+      if (d[j - money[i]] !== 10001) {
+        d[j] = Math.min(d[j], d[j - money[i]] + 1);
+      }
+    }
+  }
+
+  d[M] === 10001 ? console.log(-1) : console.log(d[15]);
+});
