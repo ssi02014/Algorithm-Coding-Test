b1f83543a9acade36401e5cbed80266b23b0104e
JeonMinJae <ssi02014@naver.com>
2021-06-13 17:56:35 +0900

    Add(minjae/baekjoon): 진법변환2

---
+++ BaekJoon/수학/진법변환2.js
@@ -0,0 +1,15 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const input = [];
+
+rl.on("line", function (line) {
+  input.push(line);
+}).on("close", function () {
+  const [N, M] = input[0].split(" ").map(Number);
+
+  console.log(N.toString(M).toUpperCase());
+});
