d28aac891575831326d097df01d6d8d81601ed9a
JeonMinJae <ssi02014@naver.com>
2021-06-13 17:14:20 +0900

    Add(minjae/baekjoon): 카드 구매하기

---
+++ BaekJoon/다이나믹프로그래밍/카드구매하기.js
@@ -0,0 +1,24 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const input = [];
+
+rl.on("line", function (line) {
+  input.push(line);
+}).on("close", function () {
+  const N = parseInt(input[0]);
+  const cards = input[1].split(" ").map(Number);
+
+  const dp = new Array(N + 1).fill(0);
+
+  for(let i = 1; i <= N; i++) {
+    for(let j = 1; j <= i; j++) {
+      dp[i] = Math.max(dp[i], dp[i - j] + cards[j - 1]);
+    }
+  }
+
+  console.log(dp);
+});
