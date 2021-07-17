d125dce2cc0e853aa6bd89108822c470f4c227ca
JeonMinJae <ssi02014@naver.com>
2021-07-18 01:13:56 +0900

    Add(minjae/baekjoon): 행렬;

--- BaekJoon/greedy/행렬.js
+++ BaekJoon/greedy/행렬.js
@@ -19,5 +19,32 @@ rl.on("line", function (line) {
 
   function solution(n, m, inputList1, inputList2) {
     let count = 0;
+
+    for (let i = 0; i < n - 2; i++) {
+      for (let j = 0; j < m - 2; j++) {
+        if (inputList1[i][j] !== inputList2[i][j]) {
+          flip(i, j, inputList1);
+          count++;
+        }
+      }
+    }
+
+    for (let i = 0; i < n; i++) {
+      for (let j = 0; j < m; j++) {
+        if (inputList1[i][j] !== inputList2[i][j]) {
+          console.log(-1);
+          return;
+        }
+      }
+    }
+    console.log(count);
+  }
+
+  function flip(x, y, inputList1) {
+    for (let i = x; i < x + 3; i++) {
+      for (let j = y; j < y + 3; j++) {
+        inputList1[i][j] = 1 - inputList1[i][j];
+      }
+    }
   }
 });
