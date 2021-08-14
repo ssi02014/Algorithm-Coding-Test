8de23a072c60752c40ee8dfd17dd114b675d1399
JeonMinJae <ssi02014@naver.com>
2021-08-14 23:43:55 +0900

    Add(minjae): 나머지 백준형식으로 변환

--- BaekJoon/배열/나머지.js
+++ BaekJoon/배열/나머지.js
@@ -1,18 +1,18 @@
-function solution(n) {
-    const temp = new Set();
-    let answer = [];
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-    // for (let i = 0; i < n.length; i++) {
-    //     remainder = n[i] % 42;
-    //     temp.add(remainder);
-    // }
+const input = [];
 
-    n.forEach(el => temp.add(el % 42));
+rl.on("line", function (line) {
+  input.push(line);
+}).on("close", function () {
+  const numList = input.slice(0).map(Number);
+  const numSet = new Set();
 
-    answer = Array.from(temp);
-    return answer.length;
-}
+  numList.map((el) => numSet.add(el % 42));
 
-console.log(solution([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
-console.log(solution([42, 84, 252, 420, 840, 126, 42, 84, 420]));
-console.log(solution([39, 40, 41, 42, 43, 44, 82, 83, 84, 85]));
+  console.log(Array.from(numSet).length);
+});
