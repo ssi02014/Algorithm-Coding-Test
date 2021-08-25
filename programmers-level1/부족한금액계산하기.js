6a468692b2c3a6947fafd3f0c00ba5a48878b0f1
JeonMinJae <ssi02014@naver.com>
2021-08-26 01:15:35 +0900

    Add(minjae): 부족한금액계산하기

---
+++ programmers-level1/부족한금액계산하기.js
@@ -0,0 +1,18 @@
+// function solution(price, money, count) {
+//     let result = 0;
+
+//     for(let i = 1; i <= count; i++) {
+//         result += price * i;
+//     }
+//     return result - money > 0 ? result - money : 0;
+// }
+
+function solution(price, money, count) {
+  let result =
+    price *
+      Array(count)
+        .fill(0)
+        .reduce((acc, _, idx) => acc + idx + 1, 0) -
+    money;
+  return result > 0 ? result : 0;
+}
