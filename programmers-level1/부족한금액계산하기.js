03bce5c9e189ca9a5d8da713460aabbe979bf314
JeonMinJae <ssi02014@naver.com>
2021-08-26 01:19:21 +0900

    Add(minjae): 2가지 방법

--- programmers-level1/부족한금액계산하기.js
+++ programmers-level1/부족한금액계산하기.js
@@ -1,11 +1,11 @@
-// function solution(price, money, count) {
-//     let result = 0;
+function solution(price, money, count) {
+  let result = 0;
 
-//     for(let i = 1; i <= count; i++) {
-//         result += price * i;
-//     }
-//     return result - money > 0 ? result - money : 0;
-// }
+  for (let i = 1; i <= count; i++) {
+    result += price * i;
+  }
+  return result - money > 0 ? result - money : 0;
+}
 
 function solution(price, money, count) {
   let result =
