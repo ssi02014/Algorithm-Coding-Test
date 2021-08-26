9912344f3fcb31c6bdb0902ece1098f3878a2e43
JeonMinJae <ssi02014@naver.com>
2021-08-27 00:09:29 +0900

    Add(minjae): 리팩토링

--- BaekJoon/이분탐색/나무자르기.js
+++ BaekJoon/이분탐색/나무자르기.js
@@ -10,18 +10,23 @@ rl.on("line", function (line) {
   input.push(line);
 }).on("close", function () {
   const [n, m] = input[0].split(" ").map(Number);
-  const woodList = input[1].split(" ").map(Number);
-  let result = 0;
+  const woodList = input[1]
+    .split(" ")
+    .map(Number)
+    .sort((a, b) => a - b);
   let [first, last] = [0, woodList[n - 1]];
+  let result = 0;
 
   while (first <= last) {
     const mid = Math.floor((first + last) / 2);
     let temp = 0;
 
-    woodList.map((el) => mid < el && (temp += el - mid));
+    woodList.map((el) => {
+      if (mid < el) temp += el - mid;
+    });
 
     if (temp >= m) {
-      result < mid && (result = mid);
+      if (result < mid) result = mid;
       first = mid + 1;
     } else {
       last = mid - 1;
