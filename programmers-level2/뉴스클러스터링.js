001533770a8149a14725f87f7cc0d92fbd59ddfc
JeonMinJae <ssi02014@naver.com>
2021-09-11 18:17:58 +0900

    Add(minjae): 뉴스클러스터링

---
+++ programmers-level2/뉴스클러스터링.js
@@ -0,0 +1,44 @@
+const filterSplitStr = (str) => {
+  const list = [];
+
+  for (let i = 0; i < str.length; i++) {
+    let temp = "";
+    let match = [];
+
+    for (let j = i + 1; j <= i + 1; j++) {
+      if (!str[j]) break;
+      temp += str[i] + str[j];
+    }
+
+    match = temp.match(/[A-Z]/gi);
+
+    if (match !== null && match.length === 2) list.push(match.join(""));
+  }
+
+  return list.sort();
+};
+
+function solution(str1, str2) {
+  const strOneUpper = str1.toUpperCase();
+  const strTwoUpper = str2.toUpperCase();
+
+  const splitOneList = filterSplitStr(strOneUpper);
+  const splitTwoList = filterSplitStr(strTwoUpper);
+
+  const union = [];
+  const intersection = [];
+
+  splitTwoList.map((el, i) => {
+    if (splitOneList.includes(el)) {
+      intersection.push(splitOneList.splice(splitOneList.indexOf(el), 1));
+    }
+    union.push(el);
+  });
+
+  splitOneList.map((el) => union.push(el));
+
+  if (!union.length) return 65536;
+  else if (!intersection.length) return 0;
+
+  return Math.floor((intersection.length / union.length) * 65536);
+}
