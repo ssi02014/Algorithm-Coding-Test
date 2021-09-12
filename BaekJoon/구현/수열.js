1a2da78f0485495442645e5e26ea41c2703bfacd
JeonMinJae <ssi02014@naver.com>
2021-09-12 19:27:54 +0900

    Add(minjae): 수열

---
+++ BaekJoon/구현/수열.js
@@ -0,0 +1,33 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const input = [];
+
+rl.on("line", function (line) {
+  input.push(line);
+}).on("close", function () {
+  const n = parseInt(input[0]);
+  const list = input[1].split(" ").map(Number);
+
+  let maxValue = 1;
+  let len = 1;
+
+  for (let i = 1; i < n; i++) {
+    if (list[i] <= list[i - 1]) len++;
+    else len = 1;
+    maxValue = Math.max(maxValue, len);
+  }
+
+  len = 1;
+
+  for (let i = 1; i < n; i++) {
+    if (list[i] >= list[i - 1]) len++;
+    else len = 1;
+    maxValue = Math.max(maxValue, len);
+  }
+
+  console.log(maxValue);
+});
