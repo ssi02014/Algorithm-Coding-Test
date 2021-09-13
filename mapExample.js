cc0673c6240491046a2f5e0378465a1d65eb4753
JeonMinJae <ssi02014@naver.com>
2021-09-13 23:54:26 +0900

    Add(minjae): map 예제

--- mapExample.js
+++ mapExample.js
@@ -1,13 +1,11 @@
-const items = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-];
+// const items = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9],
+// ];
 
-items.map((item) => {
-  console.log("item", item);
+const items = [1, 2, 3, 4, 5];
 
-  item.map((i) => {
-    console.log("i", i);
-  });
-});
+const newItems = items.map((item) => item + 1).map((item) => item * 2);
+
+console.log(newItems); // [4, 6, 8, 10, 12]
