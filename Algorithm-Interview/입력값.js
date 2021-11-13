208e1482cdaf8033c742fe993933e25f3762b89f
JeonMinJae <ssi02014@naver.com>
2021-11-13 23:41:51 +0900

    Add: 팰린드롬

---
+++ Algorithm-Interview/입력값.js
@@ -0,0 +1,11 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const input = [];
+
+rl.on("line", function (line) {
+  input.push(line);
+}).on("close", function () {});
