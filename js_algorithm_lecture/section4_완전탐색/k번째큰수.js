0ddc9ad4339a7a96b7a75d78debeaed94d091d0a
JeonMinJae <ssi02014@naver.com>
2021-12-23 19:02:49 +0900

    Add: k번째 큰수

---
+++ js_algorithm_lecture/section4_완전탐색/k번째큰수.js
@@ -0,0 +1,15 @@
+function solution(n, k, card) {
+  let set = new Set();
+
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      for (let k = j + 1; k < n; k++) {
+        set.add(card[i] + card[j] + card[k]);
+      }
+    }
+  }
+  return Array.from(set).sort((a, b) => b - a)[k - 1];
+}
+
+let arr = [13, 15, 34, 23, 45, 65, 33, 11, 26, 42];
+console.log(solution(10, 3, arr));
