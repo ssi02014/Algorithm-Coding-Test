565a7593955bf6499c6b44b824052651e0add060
JeonMinJae <ssi02014@naver.com>
2021-12-27 00:15:06 +0900

    Add: 연속부분수열2

--- js_algorithm_lecture/section5_투포인터_슬라이딩윈도우_해쉬/연속부분수열2.js
+++ js_algorithm_lecture/section5_투포인터_슬라이딩윈도우_해쉬/연속부분수열2.js
@@ -10,14 +10,29 @@ function solution(m, arr) {
     if (sum <= m) result++;
     while (sum <= m) {
       sum += arr[left++];
-      if (sum <= m) {
-        result++;
-      }
+
+      if (sum <= m) result++;
     }
   }
   return result;
 }
 
+function lectureSolution(m, arr) {
+  let result = 0;
+  let sum = 0;
+  let left = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+
+    while (sum > m) {
+      sum -= arr[left++];
+    }
+    result += i - left + 1;
+  }
+  return result;
+}
+
 let a = [1, 3, 1, 2, 3];
-console.log(solution(5, a));
-console.log(solution(5, [1, 1, 1, 1]));
+console.log(lectureSolution(5, a));
+console.log(solution(100, [10, 5, 2, 6]));
