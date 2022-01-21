e5b43494118b676c7ee4e414a4207ff762aaaf42
JeonMinJae <ssi02014@naver.com>
2022-01-22 01:08:17 +0900

    Add: 박터뜨리기

---
+++ BaekJoon/greedy/박터뜨리기.js
@@ -0,0 +1,30 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const input = [];
+
+rl.on("line", function (line) {
+  input.push(line);
+}).on("close", function () {
+  const [n, k] = input[0].split(" ").map(Number);
+  const basket = [];
+  let result = k;
+  let temp = 1;
+
+  for (let i = 0; i < k; i++) {
+    basket.push(temp++);
+  }
+
+  const min = basket.reduce((acc, cur) => acc + cur);
+
+  if (n < min) {
+    result = -1;
+  } else if ((n - min) % k === 0) {
+    result = k - 1;
+  }
+
+  console.log(result);
+});
