d341608d63bd42c99014925018edc400ae78ebc8
JeonMinJae <ssi02014@naver.com>
2022-02-04 23:47:26 +0900

    Fix: 이분검색

--- js_algorithm_lecture/section7_정렬_그리디_이분검색/이분검색.js
+++ js_algorithm_lecture/section7_정렬_그리디_이분검색/이분검색.js
@@ -4,19 +4,17 @@ function solution(target, arr) {
 
   arr.sort((a, b) => a - b);
 
-  while (start < end) {
-    let mid = Math.floor((start + end) / 2);
+  while (start <= end) {
+    const mid = Math.floor((start + end) / 2);
 
     if (arr[mid] === target) {
       return mid + 1;
     } else if (arr[mid] < target) {
-      start = mid;
+      start = mid + 1;
     } else {
-      end = mid;
+      end = mid - 1;
     }
   }
-
-  console.log(arr);
 }
 
 let arr = [23, 87, 65, 12, 57, 32, 99, 81];
