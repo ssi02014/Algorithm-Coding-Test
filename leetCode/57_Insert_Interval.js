811cd6e7964a04eee7b5765ccda7602913bc4687
JeonMinJae <ssi02014@naver.com>
2022-02-14 00:14:23 +0900

    Add: 기존 코드 Insert Interval

--- leetCode/57_Insert_Interval.js
+++ leetCode/57_Insert_Interval.js
@@ -27,3 +27,57 @@ var insert = function (intervals, newInterval) {
 
   return result;
 };
+
+// /**
+//  * @param {number[][]} intervals
+//  * @param {number[]} newInterval
+//  * @return {number[][]}
+//  */
+// var insert = function (intervals, newInterval) {
+//   const result = [];
+//   const [start, end] = newInterval;
+//   let flat = intervals.flat();
+//   let idx = 0;
+//   let newFlat = flat.filter((el) => el < start || el > end);
+
+//   if (newFlat.length < 1) {
+//     result.push(newInterval);
+//     return result;
+//   }
+
+//   if (newFlat.length % 2 !== 0) {
+//     let temp = [];
+//     let startflag = false;
+//     let endflag = false;
+
+//     for (let i = 0; i < newFlat.length; i++) {
+//       temp.push(newFlat[i]);
+
+//       if (!startflag && temp[0] > start) {
+//         temp.unshift(start);
+//         startflag = true;
+//       }
+
+//       if (!endflag && (!newFlat[i + 1] || newFlat[i + 1] > end)) {
+//         temp.push(end);
+//         endflag = true;
+//       }
+//     }
+//     newFlat = temp;
+//   } else {
+//   }
+
+//   for (let i = 0; i < Math.floor(newFlat.length / 2); i++) {
+//     let count = 0;
+//     let temp = [];
+
+//     while (temp.length < 2) {
+//       temp.push(newFlat[idx]);
+//       count++;
+//       idx++;
+//     }
+//     result.push(temp);
+//   }
+
+//   return result;
+// };
