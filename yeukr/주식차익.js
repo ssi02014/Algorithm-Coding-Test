e3999e4bd862685970715fdf1af089368a767702
JeonMinJae <ssi02014@naver.com>
2022-02-24 00:32:37 +0900

    Refacor: 주식 차익

--- yeukr/주식차익.js
+++ yeukr/주식차익.js
@@ -1,13 +1,10 @@
 function solution(arr) {
-  console.time("time");
-
-
+  console.time('time');
   let result = [];
   let left = 0;
   let right = left + 1;
 
-  while (left <= arr.length - 1) {
-
+  while (left < arr.length) {
     if (arr[left] >= arr[right]) {
       right++;
     } else if (arr[left] < arr[right]) {
@@ -22,13 +19,10 @@ function solution(arr) {
       right = left + 1;
     }
   }
-  console.timeEnd("time");
+  console.timeEnd('time');
   return result;
 }
-
 const arr = [4, 1, 4, 7, 6]; 
 console.log(solution(arr));
 console.log(solution([13, 7, 3, 7, 5, 16, 12])); 
 console.log(solution([7, 6, 1, 4, 1]));
-console.log(solution(arr.join("").repeat(100000).split("")));
-//1.315s
