c818b93d47739ba6cf75a3e6a40bb2b7667823e3
JeonMinJae <ssi02014@naver.com>
2022-03-02 01:30:11 +0900

    refac: 에디터

--- BaekJoon/스택/에디터.js
+++ BaekJoon/스택/에디터.js
@@ -1,30 +1,35 @@
-const readline = require("readline"); 
-const rl = readline.createInterface({ 
-  input: process.stdin, 
-  output: process.stdout, 
-}); 
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 const input = [];
 
 rl.on("line", function (line) {
-  input.push(line); 
-}).on("close", function () { 
+  input.push(line);
+}).on("close", function () {
   const str = input[0].split("");
-  const N = parseInt(input[1]);
+  const n = +input[1];
   const left = [];
   const right = [];
 
   for (let i = 0; i < str.length; i++) {
-      left.push(str[i]);
+    left.push(str[i]);
   }
 
-  for (let i = 2; i < N + 2; i++) {
-    let command = input[i].split(" ");
+  for (let i = 2; i < n + 2; i++) {
+    const command = input[i].split(" ");
 
-    if (command[0] === 'P') left.push(command[1]);
-    else if (command[0] === 'L' && left.length !== 0) right.push(left.pop());
-    else if (command[0] === 'D' && right.length !== 0) left.push(right.pop());
-    else if (command[0] === 'B' && left.length !== 0) left.pop();
+    if (command[0] === "P") {
+      left.push(command[1]);
+    } else if (command[0] === "L" && left.length) {
+      right.push(left.pop());
+    } else if (command[0] === "D" && right.length) {
+      left.push(right.pop());
+    } else if (command[0] === "B" && left.length) {
+      left.pop();
+    }
   }
 
   while (right.length !== 0) {
