21861730fcaf0510331dd8836555fd7684fde04f
Gromit <ssi02014@naver.com>
2022-03-29 23:04:32 +0900

    Add: 모의고사

---
+++ programmers-level1/모의고사.js
@@ -0,0 +1,27 @@
+function solution(answers) {
+  const result = [];
+  const counts = [0, 0, 0];
+  const patterns = [
+    [1, 2, 3, 4, 5],
+    [2, 1, 2, 3, 2, 4, 2, 5],
+    [3, 3, 1, 1, 2, 2, 4, 4, 5, 5],
+  ];
+
+  for (let i = 0; i < answers.length; i++) {
+    const answer = answers[i];
+
+    patterns.forEach((pattern, idx) => {
+      if (pattern[i % pattern.length] === answer) {
+        counts[idx]++;
+      }
+    });
+  }
+
+  const maxCount = Math.max(...counts);
+
+  counts.forEach((count, i) => {
+    if (count === maxCount) result.push(i + 1);
+  });
+
+  return result;
+}
