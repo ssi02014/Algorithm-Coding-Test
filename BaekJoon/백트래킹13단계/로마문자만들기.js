09f198675aea758b1058561db39325adabc2da10
Gromit <ssi02014@naver.com>
2022-04-02 23:47:54 +0900

    Add: 로마문자만들기

---
+++ BaekJoon/백트래킹13단계/로마문자만들기.js
@@ -0,0 +1,34 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const input = [];
+
+rl.on("line", function (line) {
+  input.push(line);
+}).on("close", function () {
+  const n = +input[0];
+  const number = [1, 5, 10, 50];
+  const numberList = Array(1001).fill(0);
+  let result = 0;
+
+  backTracking(0, 0, 0);
+
+  console.log(result);
+
+  function backTracking(depth, idx, s) {
+    if (depth === n) {
+      if (numberList[s] === 0) {
+        numberList[s] = 1;
+        result++;
+      }
+      return;
+    }
+
+    for (let i = idx; i < number.length; i++) {
+      backTracking(depth + 1, i, s + number[i]);
+    }
+  }
+});
