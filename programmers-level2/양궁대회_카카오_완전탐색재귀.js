4078976f7ee9525a3d3d83490a781c9a81bb3fc6
ssi02014 <ssi02014@naver.com>
2022-08-18 15:14:20 +0900

    refac: 양궁대회

--- programmers-level2/양궁대회_카카오_완전탐색재귀.js
+++ programmers-level2/양궁대회_카카오_완전탐색재귀.js
@@ -8,37 +8,39 @@ function solution(n, info) {
   if (max <= 0) return [-1];
   return result;
 
-  function shot(peachScore, ryanScore, count, idx, scoreBoard) {
-    // count가 n보다 커지면 재귀 종료
+  function shot(count, idx, peachScore, ryanScore, scoreBoard) {
+    // count가 n보다 커지면 모든 화살을 쏜 것이기때문에 재귀 종료
     if (n < count) return;
-    // idx가 10이면 스코어 계산 후 종료
+    // idx가 10보다 커지면 어차피 남은 화살들은 0점이기 떄문에 0점에 추가하고 스코어 계산 후 재귀 종료
     if (idx > 10) {
-      const scoreDiff = ryanScore - peachScore;
+      const diffScore = ryanScore - peachScore;
 
-      if (max < scoreDiff) {
-        scoreBoard[10] = n - count;
-        max = scoreDiff;
+      if (max < diffScore) {
+        // max값 diffScore이 더 크다면 라이언이 더 높은 점수로 이길 수 있다는 뜻이기 때문에 교체한다.
+        scoreBoard[10] = n - count; // 남은 화살 0점에다 넣기
+        max = diffScore;
         result = scoreBoard;
       }
       return;
     }
 
     if (n > count) {
-      const candidate = [...scoreBoard];
-      candidate[10 - idx] = info[10 - idx] + 1;
+      const cloneScoreBoard = [...scoreBoard];
+      // 작은값부터 체크하면서 최대값을 구해야하기 때문에 뒤에서부터 계산
+      const score = 10 - idx;
+      cloneScoreBoard[score] = info[score] + 1;
       shot(
+        count + cloneScoreBoard[score],
+        idx + 1,
         peachScore,
         ryanScore + idx,
-        count + info[10 - idx] + 1,
-        idx + 1,
-        candidate
+        cloneScoreBoard
       );
     }
-
     if (info[10 - idx] > 0) {
-      shot(peachScore + idx, ryanScore, count, idx + 1, scoreBoard);
+      shot(count, idx + 1, peachScore + idx, ryanScore, scoreBoard);
     } else {
-      shot(peachScore, ryanScore, count, idx + 1, scoreBoard);
+      shot(count, idx + 1, peachScore, ryanScore, scoreBoard);
     }
   }
 }
