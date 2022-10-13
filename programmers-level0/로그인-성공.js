ac6bf9a51bcd0f713f3f864e1e9c68a02b5e3595
ssi02014 <ssi02014@naver.com>
2022-10-14 00:13:26 +0900

    add: 로그인 성공 문제

---
+++ programmers-level0/로그인-성공.js
@@ -0,0 +1,8 @@
+function solution(id_pw, db) {
+  const [id, pw] = id_pw;
+  const filteredId = db.filter((data) => data[0] === id)[0];
+
+  if (!filteredId) return "fail";
+  if (filteredId[1] !== pw) return "wrong pw";
+  return "login";
+}
