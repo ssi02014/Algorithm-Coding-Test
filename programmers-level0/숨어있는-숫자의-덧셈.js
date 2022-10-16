2239ed7be3db98a4cce90fb074984fd8f204d2a3
ssi02014 <ssi02014@naver.com>
2022-10-16 20:52:53 +0900

    refac: 숨어있는 숫자의 덧셈

--- programmers-level0/숨어있는-숫자의-덧셈.js
+++ programmers-level0/숨어있는-숫자의-덧셈.js
@@ -2,6 +2,5 @@ function solution(my_string) {
   return my_string
     .replace(/[^0-9]/g, " ")
     .split(" ")
-    .map(Number)
-    .reduce((acc, cur) => acc + cur, 0);
+    .reduce((acc, cur) => acc + Number(cur), 0);
 }
