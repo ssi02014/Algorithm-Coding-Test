a629fde5ce89d1c939742b6563a801d3610a95b7
ssi02014 <ssi02014@naver.com>
2022-10-31 17:51:19 +0900

    add: OX퀴즈 리팩

--- programmers-level0/OX퀴즈.js
+++ programmers-level0/OX퀴즈.js
@@ -1,12 +1,12 @@
 function solution(quiz) {
   return quiz.map((el) => {
-    const [formula, result] = el.split(" = ");
-    const [operand1, operator, operand2] = formula
+    const [operand1, operator, operand2, equalSign, formulaResult] = el
       .split(" ")
       .map((el) => (isNaN(+el) ? el : +el));
-    const temp =
+
+    const sum =
       operator === "-" ? operand1 + operand2 * -1 : operand1 + operand2;
 
-    return temp === +result ? "O" : "X";
+    return sum === formulaResult ? "O" : "X";
   });
 }
