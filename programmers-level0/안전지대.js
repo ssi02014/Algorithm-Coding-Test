9bfca0e3e20c2f91ea948ece32b590e81138761a
ssi02014 <ssi02014@naver.com>
2022-11-02 23:12:04 +0900

    add: 안전 지대

---
+++ programmers-level0/안전지대.js
@@ -0,0 +1,62 @@
+function solution(board) {
+  let boardSize = 0;
+  let dangerAreaCount = 0;
+
+  for (let i = 0; i < board.length; i++) {
+    boardSize += board[i].length;
+
+    for (let j = 0; j < board[i].length; j++) {
+      if (board[i][j] === 1) {
+        dangerAreaCount = changeDangerArea(++dangerAreaCount, board, i, j);
+      }
+    }
+  }
+
+  return boardSize - dangerAreaCount;
+}
+
+function changeDangerArea(dangerAreaCount, board, x, y) {
+  const dx = [-1, -1, -1, 0, 0, 1, 1, 1];
+  const dy = [1, 0, -1, 1, -1, 1, 0, -1];
+
+  for (let i = 0; i < dx.length; i++) {
+    const [nx, ny] = [x + dx[i], y + dy[i]];
+
+    if (isValid(board, nx, ny)) {
+      board[nx][ny] = 2;
+      dangerAreaCount++;
+    }
+  }
+
+  return dangerAreaCount;
+}
+
+function isValid(board, x, y) {
+  if (
+    x < 0 ||
+    y < 0 ||
+    x > board.length - 1 ||
+    y > board.length - 1 ||
+    board[x][y] === 1 ||
+    board[x][y] === 2
+  ) {
+    return false;
+  }
+  return true;
+}
+
+/*
+0 0 0 0 0
+0 0 0 0 0
+0 0 0 0 0
+0 0 1 0 0
+0 0 0 0 0
+= 16
+
+0 0 0 0 0
+0 0 0 0 0
+0 0 0 0 0
+0 0 1 1 0
+0 0 0 0 0
+= 13
+*/
