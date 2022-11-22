de780a8885ada5708ee491e6156fbfe8787191dc
ssi02014 <ssi02014@naver.com>
2022-11-23 00:15:31 +0900

    refac: 숫자문자열과영단어 리팩토링

--- programmers-level1/숫자문자열과영단어.js
+++ programmers-level1/숫자문자열과영단어.js
@@ -29,3 +29,22 @@ function solution(s) {
 
   return Number(result);
 }
+
+// refactor
+function solution(s) {
+  const numberMappingObj = {
+    zero: 0,
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+  };
+  const regex = /(zero|one|two|three|four|five|six|seven|eight|nine)/g;
+
+  return +s.replace(regex, (el) => numberMappingObj[el]);
+}
