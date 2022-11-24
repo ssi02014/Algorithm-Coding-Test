b9f0304d6a817250ba428d8b0a498d3291cc70dd
ssi02014 <ssi02014@naver.com>
2022-11-25 01:30:15 +0900

    refac: 로또의 최고 순위와 최저 순위 리팩토링

--- programmers-level1/로또의최고순위와최저순위.js
+++ programmers-level1/로또의최고순위와최저순위.js
@@ -1,32 +1,8 @@
 function solution(lottos, win_nums) {
-  const result = [];
+  const rank = [6, 6, 5, 4, 3, 2, 1];
+  const winCount = lottos.filter((lotto) => win_nums.includes(lotto)).length;
+  const zeroCount = lottos.filter((lotto) => !lotto).length;
+  const bestCount = winCount + zeroCount;
 
-  let [topRank, lowestRank] = [0, 0];
-
-  lottos.map((el) => {
-    if (win_nums.includes(el)) {
-      topRank++;
-      lowestRank++;
-    }
-
-    if (el === 0) {
-      topRank++;
-    }
-  });
-
-  if (topRank === 6) result.push(1);
-  else if (topRank === 5) result.push(2);
-  else if (topRank === 4) result.push(3);
-  else if (topRank === 3) result.push(4);
-  else if (topRank === 2) result.push(5);
-  else result.push(6);
-
-  if (lowestRank === 6) result.push(1);
-  else if (lowestRank === 5) result.push(2);
-  else if (lowestRank === 4) result.push(3);
-  else if (lowestRank === 3) result.push(4);
-  else if (lowestRank === 2) result.push(5);
-  else result.push(6);
-
-  return result;
+  return [rank[bestCount], rank[winCount]];
 }
