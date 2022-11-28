f74a6be2c79fabd828fba56a29077a49a1f07f9c
ssi02014 <ssi02014@naver.com>
2022-11-28 21:43:50 +0900

    refac: 다트게임 리팩토링

--- programmers-level1/다트게임.js
+++ programmers-level1/다트게임.js
@@ -1,22 +1,29 @@
+function getMatchedString(str) {
+  const bonus = str.match(/[S|D|T]{1}/g)?.[0];
+  const option = str.match(/[*|#]/g)?.[0];
+  const point = +str.match(/\d{1,2}/g)?.[0];
+
+  return [point, bonus, option];
+}
+
 function solution(dartResult) {
-    const answer = [];
-    let temp = 0;
-    
-    for (let i = 0; i < dartResult.length; i++) {
-        if (dartResult[i] >= 0 && dartResult[i] <= 9) {
-            if (dartResult[i] === '1' && dartResult[i + 1] === '0') {
-                temp = 10;
-                i++;
-            } else temp = parseInt(dartResult[i]);
-        } 
-        else if (dartResult[i] === 'S') answer.push(temp);
-        else if (dartResult[i] === 'D') answer.push(Math.pow(temp, 2));
-        else if (dartResult[i] === 'T') answer.push(Math.pow(temp, 3));
-        else if (dartResult[i] === '#') answer[answer.length - 1] *= -1;
-        else if (dartResult[i] === '*') {
-            answer[answer.length - 1] *= 2;
-            answer[answer.length - 2] *= 2;
-        }
-    }
-    return (answer.reduce((a, b) => a + b));
+  const regex = /\d{1,2}[S|D|T]{1}[*|#]?/g;
+  const bonusObj = { S: 1, D: 2, T: 3 };
+  const optionObj = { "*": 2, "#": -1 };
+
+  const splitDartResult = dartResult.match(regex);
+  const result = [];
+
+  splitDartResult.forEach((el, i) => {
+    let [point, bonus, option] = getMatchedString(el);
+
+    point **= bonusObj[bonus];
+    point *= optionObj[option] || 1;
+
+    if (option === "*") result[i - 1] *= 2;
+
+    result.push(point);
+  });
+
+  return result.reduce((acc, cur) => acc + cur, 0);
 }
