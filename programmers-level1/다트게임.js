8013451344ae69d549e46d9af2d1c6eaa61e21b2
ssi02014 <ssi02014@naver.com>
2022-11-29 01:33:42 +0900

    refac: 다트 게임 리팩토링

--- programmers-level1/다트게임.js
+++ programmers-level1/다트게임.js
@@ -7,19 +7,22 @@ function getMatchedString(str) {
 }
 
 function solution(dartResult) {
-  const regex = /\d{1,2}[S|D|T]{1}[*|#]?/g;
+  const allRegex = /\d{1,2}[S|D|T]{1}[*|#]?/g;
   const bonusObj = { S: 1, D: 2, T: 3 };
   const optionObj = { "*": 2, "#": -1 };
 
-  const splitDartResult = dartResult.match(regex);
+  // allRegex를 이용해 1S2D*3T인 경우 ["1S", "2D*", "3T"]로 분리
+  const splittedDartResult = dartResult.match(allRegex);
   const result = [];
 
-  splitDartResult.forEach((el, i) => {
+  splittedDartResult.forEach((el, i) => {
     let [point, bonus, option] = getMatchedString(el);
 
+    // 객체로 매핑해서 반복되는 코드 개선
     point **= bonusObj[bonus];
     point *= optionObj[option] || 1;
 
+    // *인 경우 그 앞에있는 요소도 2배해줘야 되기 때문에 이를 대응 코드
     if (option === "*") result[i - 1] *= 2;
 
     result.push(point);
