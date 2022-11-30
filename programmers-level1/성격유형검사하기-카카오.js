8743afa5156da2ab86ff61a05d2f6a80cd5ba5ea
ssi02014 <ssi02014@naver.com>
2022-11-30 17:45:16 +0900

    refac: 성격 유형 검사 카카오 리팩토링

--- programmers-level1/성격유형검사하기-카카오.js
+++ programmers-level1/성격유형검사하기-카카오.js
@@ -1,5 +1,21 @@
+function getResultPersonalityType(pointBoard) {
+  const pointBoardEntries = Object.entries(pointBoard);
+  let result = "";
+
+  // 현재 타입과 다음 타입의 포인트를 비교해 최종 타입을 result에 적용
+  for (let i = 0; i < pointBoardEntries.length; i += 2) {
+    const [curType, curPoint] = pointBoardEntries[i];
+    const [nextType, nextPoint] = pointBoardEntries[i + 1];
+    const finalType = curPoint < nextPoint ? nextType : curType;
+
+    result += finalType;
+  }
+
+  return result;
+}
+
 function solution(survey, choices) {
-  const points = [3, 2, 1, 0, 1, 2, 3];
+  const points = [0, 3, 2, 1, 0, 1, 2, 3];
   const pointBoard = {
     R: 0,
     T: 0,
@@ -10,30 +26,13 @@ function solution(survey, choices) {
     A: 0,
     N: 0,
   };
-  let result = "";
 
-  for (let i = 0; i < survey.length; i++) {
-    const categories = survey[i];
+  // 타입 별 포인트 합산
+  choices.forEach((choice, i) => {
+    const selectedType = choice <= 4 ? survey[i][0] : survey[i][1];
 
-    if (choices[i] < 4) {
-      pointBoard[categories[0]] += points[choices[i] - 1];
-    } else if (choices[i] > 4) {
-      pointBoard[categories[1]] += points[choices[i] - 1];
-    }
-  }
+    pointBoard[selectedType] += points[choice];
+  });
 
-  const pointBoardEntries = Object.entries(pointBoard);
-
-  for (let i = 0; i < pointBoardEntries.length; i += 2) {
-    const [curCategory, curValue] = pointBoardEntries[i];
-    const [nextCategory, nextValue] = pointBoardEntries[i + 1];
-
-    if (curValue < nextValue) {
-      result += nextCategory;
-    } else {
-      result += curCategory;
-    }
-  }
-
-  return result;
+  return getResultPersonalityType(pointBoard);
 }
