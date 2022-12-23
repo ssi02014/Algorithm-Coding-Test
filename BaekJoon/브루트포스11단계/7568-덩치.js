abc233251fb207739c49e1c8d956112054bdb6ba
ssi02014 <ssi02014@naver.com>
2022-12-23 23:58:26 +0900

    refac: 7658 덩치

---
+++ BaekJoon/브루트포스11단계/7568-덩치.js
@@ -0,0 +1,33 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const input = [];
+
+rl.on("line", function (line) {
+  input.push(line);
+}).on("close", function () {
+  const n = +input.shift();
+  const studentList = input.map((el) => el.split(" ").map(Number));
+  const result = [];
+
+  for (let i = 0; i < n; i++) {
+    let rank = 0;
+
+    for (let j = 0; j < n; j++) {
+      const [curtWeight, curHeight] = studentList[i];
+      const [nextWeight, nextHeight] = studentList[j];
+
+      if (i === j) continue;
+      if (curtWeight < nextWeight && curHeight < nextHeight) {
+        rank++;
+      }
+    }
+
+    result.push(rank + 1);
+  }
+
+  console.log(result.join(" "));
+});
