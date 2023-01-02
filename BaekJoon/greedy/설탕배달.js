d136752bd12decd8472490fcb95a915535e42362
ssi02014 <ssi02014@naver.com>
2023-01-02 23:37:48 +0900

    refac: 설탕배달 풀이

--- BaekJoon/greedy/설탕배달.js
+++ BaekJoon/greedy/설탕배달.js
@@ -1,30 +1,27 @@
-const readline = require("readline"); 
-const rl = readline.createInterface({ 
-  input: process.stdin, 
-  output: process.stdout, 
-}); 
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 const input = [];
 
 rl.on("line", function (line) {
-  input.push(line); 
-}).on("close", function () { 
-    let N = parseInt(input[0]);
-    let count = 0;
+  input.push(line);
+}).on("close", function () {
+  let n = parseInt(input[0]);
+  let count = 0;
 
-    while (true) {
-        if (N % 5 === 0) {
-            count += N / 5;
-            console.log(count);
-            break;
-        }
-
-        N -= 3;
-        count++;
-
-        if (N < 0) {
-            console.log('-1');
-            break;
-        }
+  while (true) {
+    if (n < 0) break;
+    if (n % 5 === 0) {
+      count += n / 5;
+      break;
     }
+
+    n -= 3;
+    count++;
+  }
+
+  console.log(n < 0 ? -1 : count);
 });
