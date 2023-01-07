a74ebad116594d690328627f816efeb793a7d434
ssi02014 <ssi02014@naver.com>
2023-01-08 03:29:24 +0900

    refac: 개인정보수집유효기간 리팩토링

--- programmers-level1/개인정보-수집-유효기간-kakao.js
+++ programmers-level1/개인정보-수집-유효기간-kakao.js
@@ -3,22 +3,19 @@ function solution(today, terms, privacies) {
   const splittedTerms = terms.map((el) => el.split(" "));
 
   const insertZero = (date) => (date < 10 ? `0${date}` : date);
-
-  const privacyDates = privacies.map((privacy) => {
-    const [privacyDate, privacyType] = privacy.split(" ");
-    const validDate = +splittedTerms.find((term) => term[0] === privacyType)[1];
+  const calcDate = (privacyDate, validDate) => {
     let [year, month, date] = privacyDate.split(".").map(Number);
 
     month += validDate;
     date -= 1;
 
-    // calculate Date
+    // date이 1보다 작은 케이스
     if (date < 1) {
       date = 28;
       month -= 1;
     }
 
-    // calculate Month
+    // month가 12보다 큰 케이스
     if (month > 12) {
       year += Math.floor(month / 12);
       month %= 12;
@@ -28,6 +25,15 @@ function solution(today, terms, privacies) {
         year -= 1;
       }
     }
+
+    return [year, month, date];
+  };
+
+  const privacyDates = privacies.map((privacy) => {
+    const [privacyDate, privacyType] = privacy.split(" ");
+    const validDate = +splittedTerms.find((term) => term[0] === privacyType)[1];
+
+    const [year, month, date] = calcDate(privacyDate, validDate);
 
     return `${year}${insertZero(month)}${insertZero(date)}`;
   });
