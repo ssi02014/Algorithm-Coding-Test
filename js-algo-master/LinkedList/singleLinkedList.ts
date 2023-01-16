02b3a37e20c9cadb81660aa44c4e748a9ae9df94
ssi02014 <ssi02014@naver.com>
2023-01-17 01:28:08 +0900

    add: shift, unshift 추가

--- js-algo-master/LinkedList/singleLinkedList.ts
+++ js-algo-master/LinkedList/singleLinkedList.ts
@@ -20,18 +20,21 @@ class SingleLinkedList {
   }
 
   public push(val: any) {
+    // newNode 생성
     const newNode = new NodeItem(val);
 
-    // 최초 노드 삽입
     if (!this.head) {
+      // 최초 노드 삽입, head, tail 모두 newNode를 가리킴
       this.head = newNode;
-      this.tail = this.head; // tail한테 this.head를 할당(주소 참조)
+      this.tail = this.head; // 주소 참조
     } else {
+      // 요소가 뒤에 추가되니 tail의 next를 먼저
       this.tail.next = newNode;
       this.tail = newNode;
     }
 
     this.length += 1;
+
     return this;
   }
 
@@ -41,13 +44,15 @@ class SingleLinkedList {
     let current = this.head;
     let newTail = current;
 
+    // current가 연결리스트의 마지막 요소까지 이동시킴
+    // 즉 next에 아무것도 없을 때까지
     while (current.next) {
       newTail = current;
       current = current.next;
     }
 
-    // current는 기존 tail Node를 바라봄
-    // newTail은 기존 tail Node의 바로 이전 Node를 바라봄
+    // current는 기존 tail Node를 바라봄(제거해야 되는 노드)
+    // newTail은 기존 tail Node의 바로 이전 Node를 바라봄(마지막이 될 노드)
     this.tail = newTail;
     this.tail.next = null;
     this.length -= 1;
@@ -61,6 +66,42 @@ class SingleLinkedList {
     // 제거 된 기존 tail Node를 리턴, 배열 메서드의 pop도 제거된 요소를 반환 함
     return current;
   }
+
+  public shift() {
+    // head가 없으면 undefined 반환
+    if (!this.head) return;
+
+    const currentHead = this.head;
+
+    this.head = currentHead.next;
+    this.length--;
+
+    // 길이가0이면 tail을 null로 셋팅
+    if (!this.length) {
+      this.tail = null;
+    }
+
+    return currentHead;
+  }
+
+  public unshift(val: any) {
+    const newNode = new NodeItem(val);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+
+    this.length++;
+    return this;
+  }
+
+  public size() {
+    return this.length;
+  }
 }
 
 const linkedList = new SingleLinkedList();
@@ -68,8 +109,5 @@ linkedList.push("Hello");
 linkedList.push("GoodBye");
 linkedList.push("!!");
 
-linkedList.pop();
-linkedList.pop();
-linkedList.pop();
-linkedList.pop();
 console.log(linkedList);
+console.log(linkedList.size());
