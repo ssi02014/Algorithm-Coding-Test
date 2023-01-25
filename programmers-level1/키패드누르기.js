4648be94a57a6a99e072ef37268b4dad3de71bf8
ssi02014 <ssi02014@naver.com>
2023-01-26 01:54:39 +0900

    fix: 키패드 누르기

--- programmers-level1/키패드누르기.js
+++ programmers-level1/키패드누르기.js
@@ -1,64 +1,72 @@
-function findPosition(keyboard, number) {
-  let handPosition = [];
-
-  for (let i = 0; i < keyboard.length; i++) {
-    for (let j = 0; j < keyboard[i].length; j++) {
-      if (keyboard[i][j] === number) handPosition = [i, j];
-    }
-  }
-
-  return handPosition;
-}
-
 function solution(numbers, hand) {
-  const keyboard = [
-    ["*", 0, "#"],
-    [7, 8, 9],
-    [4, 5, 6],
+  const keypad = [
     [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    ["*", 0, "#"],
   ];
+  const [keypadHeight, keypadWidth] = [keypad.length, keypad[0].length];
+  const fixedHandTypeObj = {
+    1: "left",
+    4: "left",
+    7: "left",
+    3: "right",
+    6: "right",
+    9: "right",
+  };
 
+  // 손가락 초기값 위치 SET
   let [leftHand, rightHand] = [
-    [0, 0],
-    [0, 2],
+    [3, 0],
+    [3, 2],
   ];
-  let [leftDist, rightDist] = [0, 0];
-  let centerNum = [];
-  let result = "";
 
-  numbers.map((number) => {
-    if (number === 1 || number === 4 || number === 7) {
-      result += "L";
-      leftHand = findPosition(keyboard, number);
-    } else if (number === 3 || number === 6 || number === 9) {
-      result += "R";
-      rightHand = findPosition(keyboard, number);
-    } else {
-      centerNum = findPosition(keyboard, number);
-      leftDist =
-        Math.abs(leftHand[0] - centerNum[0]) +
-        Math.abs(leftHand[1] - centerNum[1]);
-      rightDist =
-        Math.abs(rightHand[0] - centerNum[0]) +
-        Math.abs(rightHand[1] - centerNum[1]);
-
-      if (leftDist > rightDist) {
-        result += "R";
-        rightHand = centerNum;
-      } else if (leftDist < rightDist) {
-        result += "L";
-        leftHand = centerNum;
-      } else {
-        if (hand === "left") {
-          result += "L";
-          leftHand = centerNum;
-        } else {
-          result += "R";
-          rightHand = centerNum;
+  // 누른 버튼의 위치 GET
+  const getPressedNumberPosition = (number) => {
+    for (let i = 0; i < keypadHeight; i++) {
+      for (let j = 0; j < keypadWidth; j++) {
+        if (keypad[i][j] === number) {
+          return [i, j];
         }
       }
     }
+  };
+
+  // 현재 손가락과 버튼과의 거리 GET
+  const getDiffDistance = (position1, position2) => {
+    return (
+      Math.abs(position1[0] - position2[0]) +
+      Math.abs(position1[1] - position2[1])
+    );
+  };
+
+  const onChangeHandPosition = {
+    left: (position) => {
+      leftHand = position;
+      return "L";
+    },
+    right: (position) => {
+      rightHand = position;
+      return "R";
+    },
+  };
+
+  const result = numbers.map((number) => {
+    const pressedNumPosition = getPressedNumberPosition(number);
+    let handType = fixedHandTypeObj[number];
+
+    if (!handType) {
+      const leftDist = getDiffDistance(leftHand, pressedNumPosition);
+      const rightDist = getDiffDistance(rightHand, pressedNumPosition);
+
+      if (leftDist === rightDist) {
+        handType = hand;
+      } else {
+        handType = leftDist < rightDist ? "left" : "right";
+      }
+    }
+    return onChangeHandPosition[handType](pressedNumPosition);
   });
 
-  return result;
+  return result.join("");
 }
