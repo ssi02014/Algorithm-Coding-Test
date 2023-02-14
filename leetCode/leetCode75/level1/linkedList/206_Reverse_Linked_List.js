42a694710c21b726f4a1ed7ed8209bbf9b6ca02d
ssi02014 <ssi02014@naver.com>
2023-02-14 21:50:33 +0900

    add: Leetcode 21, 206

---
+++ leetCode/leetCode75/level1/linkedList/206_Reverse_Linked_List.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const reverseList = (head) => {
+  let current = head;
+  let prev = null;
+  let next = null;
+
+  while (current) {
+    next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
+  }
+
+  return prev;
+};
---
+++ leetCode/leetCode75/level1/linkedList/21_Merge_Two_Sorted_Lists.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+
+function ListNode(val, next) {
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+}
+const mergeTwoLists = (list1, list2) => {
+  const head = new ListNode(null);
+  let current = head;
+
+  // loop while both lists are not null (did not reach end)
+  while (list1 && list2) {
+    if (list1.val <= list2.val) {
+      current.next = list1;
+      list1 = list1.next;
+    } else {
+      current.next = list2;
+      list2 = list2.next;
+    }
+
+    // move the current pointer
+    current = current.next;
+  }
+
+  current.next = list1 || list2;
+
+  return head.next;
+};
