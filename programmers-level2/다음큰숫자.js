8dd3843294223a2ad14e2b1b00a1b8e0385afdae
ssi02014 <ssi02014@naver.com>
2023-02-24 00:49:04 +0900

    refac: 다음 큰 숫자 풀이

--- programmers-level2/다음큰숫자.js
+++ programmers-level2/다음큰숫자.js
@@ -1,24 +1,20 @@
+function getOneCount(str) {
+  let result = 0;
+
+  for (const char of str) {
+    if (char === "1") result++;
+  }
+  return result;
+}
+
 function solution(n) {
-    let answer = 0;
-    let count = 0;
-    let binaryNumber = n.toString(2);
-    
-    for(let i = 0; i < binaryNumber.length; i++) {
-        if (binaryNumber[i] === '1') count++; 
-    }
-    
-    while (true) {
-        let answerCount = 0;
-        
-        n += 1;
-        binaryNumber = n.toString(2);
-        
-        for (let i = 0; i < binaryNumber.length; i++) {
-            if (binaryNumber[i] === '1') answerCount++; 
-        }
-        
-        if (answerCount === count) {
-            return answer = n;
-        }
-    }
+  const binaryNum = n.toString(2);
+  const targetCount = getOneCount(binaryNum);
+
+  while (true) {
+    const nextBinaryNum = (++n).toString(2);
+    const nextCount = getOneCount(nextBinaryNum);
+
+    if (nextCount === targetCount) return n;
+  }
 }
