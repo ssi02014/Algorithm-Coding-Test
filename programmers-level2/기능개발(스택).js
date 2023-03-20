2e5b131bebe6f8bb4fb27da805d1fe2c7e05b274
ssi02014 <ssi02014@naver.com>
2023-03-20 18:15:18 +0900

    refac: 기능 개발 리팩토링

--- programmers-level2/기능개발(스택).js
+++ programmers-level2/기능개발(스택).js
@@ -1,23 +1,21 @@
 function solution(progresses, speeds) {
-    const answer = [];
-    
-    //Queue
-    while (speeds.length > 0) {
-        for (let i = 0; i < speeds.length; i++) {
-            if (progresses[i] < 100) {
-                progresses[i] += speeds[i];
-            }
-        }
-        
-        let count = 0;
-        while (progresses[0] >= 100) {
-            progresses.shift();
-            speeds.shift();
-            count++;
-        }
-        if (count > 0) {
-            answer.push(count);
-        }
+  const result = [];
+
+  while (progresses.length) {
+    let count = 0;
+
+    progresses.forEach((progress, idx) => {
+      progresses[idx] += speeds[idx];
+    });
+
+    while (progresses[0] >= 100) {
+      progresses.shift();
+      speeds.shift();
+      count++;
     }
-    return answer;
+
+    count && result.push(count);
+  }
+
+  return result;
 }
