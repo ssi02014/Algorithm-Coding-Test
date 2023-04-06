41e4912c9a5abbb4294dfac221fa86e34da9c338
ssi02014 <ssi02014@naver.com>
2023-04-06 22:37:57 +0900

    refac: n진수 게임 리팩토링

--- programmers-level2/n진수게임.js
+++ programmers-level2/n진수게임.js
@@ -1,15 +1,14 @@
 function solution(n, t, m, p) {
+  const MAX_RANGE = t * m;
+  let text = "";
   let result = "";
-  let numbers = "";
-  let idx = p - 1;
 
-  for (let i = 0; i <= m * t; i++) {
-    numbers += i.toString(n).toUpperCase();
+  for (let i = 0; i < MAX_RANGE; i++) {
+    text += i.toString(n).toUpperCase();
   }
 
-  for (let i = 0; i < t; i++) {
-    result += numbers[idx];
-    idx += m;
+  for (let i = p - 1; i < MAX_RANGE; i += m) {
+    result += text[i];
   }
 
   return result;
