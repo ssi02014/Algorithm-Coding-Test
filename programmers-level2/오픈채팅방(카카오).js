a47068a63d4a72d233d271db980aae8b0f92a60d
ssi02014 <ssi02014@naver.com>
2023-04-21 03:03:38 +0900

    refac: 오픈채팅방

--- programmers-level2/오픈채팅방(카카오).js
+++ programmers-level2/오픈채팅방(카카오).js
@@ -1,28 +1,25 @@
 function solution(records) {
-  const result = [];
-  const temp = {};
+  const stack = [];
+  const nicknameMappingObj = {};
 
-  let elements = [];
+  records.forEach((record) => {
+    const [inOut, uid, nickname] = record.split(" ");
 
-  records.map((record) => {
-    elements = record.split(" ");
+    if (inOut !== "Change") {
+      stack.push([inOut, uid]);
+    }
 
-    for (let i = 0; i < elements.length; i++) {
-      if (elements[0] === "Enter" || elements[0] === "Change") {
-        temp[elements[1]] = elements[2];
-      }
+    if (nickname) {
+      nicknameMappingObj[uid] = nickname;
     }
   });
 
-  records.map((record) => {
-    elements = record.split(" ");
+  return stack.map((el) => {
+    const [inOut, uid] = el;
 
-    if (elements[0] === "Enter") {
-      result.push(`${temp[elements[1]]}님이 들어왔습니다.`);
-    } else if (elements[0] === "Leave") {
-      result.push(`${temp[elements[1]]}님이 나갔습니다.`);
+    if (inOut === "Enter") {
+      return `${nicknameMappingObj[uid]}님이 들어왔습니다.`;
     }
+    return `${nicknameMappingObj[uid]}님이 나갔습니다.`;
   });
-
-  return result;
 }
