3ad77eaa2899652bafc92851932285c6ec9fba7c
ssi02014 <ssi02014@naver.com>
2023-05-26 01:51:47 +0900

    refac: 숨바꼭질

--- BaekJoon/DFS-BFS/1697-숨바꼭질-bfs.js
+++ BaekJoon/DFS-BFS/1697-숨바꼭질-bfs.js
@@ -12,31 +12,29 @@ rl.on("line", function (line) {
 }).on("close", function () {
   const [n, k] = input[0].split(" ").map(Number);
   const visited = Array(100001).fill(false);
-  const getMoves = (curNode) => [curNode + 1, curNode - 1, curNode * 2];
+  const getNextNodes = (curNode) => [curNode + 1, curNode - 1, curNode * 2];
 
-  const checkRange = (move) => {
-    if (move < 0 || move > 100000) {
+  const getIsValid = (node) => {
+    if (node < 0 || node > 100000 || visited[node]) {
       return false;
     }
     return true;
   };
 
-  const bfs = (n) => {
-    const queue = [];
+  const bfs = (startNode) => {
+    const queue = [[startNode, 0]];
 
-    // startNode, time
-    queue.push([n, 0]);
-    visited[n] = true;
+    visited[startNode] = true;
 
     while (queue.length) {
       const [curNode, time] = queue.shift();
 
       if (curNode === k) return time; // 중단점
 
-      for (const nextMove of getMoves(curNode)) {
-        if (!visited[nextMove] && checkRange(nextMove)) {
-          visited[nextMove] = true;
-          queue.push([nextMove, time + 1]);
+      for (const nextNode of getNextNodes(curNode)) {
+        if (getIsValid(nextNode)) {
+          visited[nextNode] = true;
+          queue.push([nextNode, time + 1]);
         }
       }
     }
