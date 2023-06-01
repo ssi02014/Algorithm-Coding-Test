c550dbe08c77ea90b63bb294ce5b0e40b22b4544
ssi02014 <ssi02014@naver.com>
2023-06-01 23:58:45 +0900

    feat: 모음 사전

--- programmers-level2/모음사전.js
+++ programmers-level2/모음사전.js
@@ -1,16 +1,69 @@
+// 수학적 풀이
 function solution(word) {
   const words = ["A", "E", "I", "O", "U"];
   let result = 0;
 
-  word.split("").map((el, i) => {
-    if (el === "A") result += 1;
-    else {
-      for (let j = 4; j > i; j--) {
-        result += 5 ** (j - i) * words.indexOf(el);
-      }
-      result += words.indexOf(el) + 1;
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === "A") {
+      result += 1;
+      continue;
     }
-  });
+
+    for (let j = 4; j > i; j--) {
+      result += 5 ** (j - i) * words.indexOf(word[i]);
+    }
+    result += words.indexOf(word[i]) + 1;
+  }
 
   return result;
+}
+
+// 완전 탐색1
+function solution(word) {
+  const vowels = ["A", "E", "I", "O", "U"];
+  const result = [];
+  const str = "";
+
+  const dfs = (word, length) => {
+    if (word.length === length) {
+      result.push(word);
+      return;
+    }
+
+    vowels.forEach((vowel) => {
+      dfs(word + vowel, length);
+    });
+  };
+
+  for (let i = 1; i <= 5; i++) {
+    dfs(str, i);
+  }
+
+  return result.sort().indexOf(word) + 1;
+}
+
+// 완전 탐색2
+function solution(word) {
+  const dictionary = ["A", "E", "I", "O", "U"];
+  let result = 0;
+  let isValid = false;
+
+  const dfs = (string) => {
+    if (!isValid) {
+      result++;
+
+      if (string === word) {
+        isValid = true;
+        return;
+      }
+
+      if (string.length < 5) {
+        dictionary.forEach((el) => dfs(string + el));
+      }
+    }
+  };
+
+  dfs("");
+
+  return --result;
 }
