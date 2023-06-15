d3e97cc090fd3ac86f37c6f3a005b5759a8699c3
ssi02014 <ssi02014@naver.com>
2023-06-16 00:25:55 +0900

    refac: 2개 이하로 다른 비트

--- programmers-level2/2개이하로다른비트.js
+++ programmers-level2/2개이하로다른비트.js
@@ -1,62 +1,22 @@
-// function solution(numbers) {
-//   let result = [];
-
-//   numbers.forEach((number) => {
-//     let binaryCurNumber = number.toString(2);
-//     let nextNumber = number + 1;
-
-//     while (true) {
-//       const binaryNextNumber = nextNumber.toString(2);
-//       const diffLength = binaryNextNumber.length - binaryCurNumber.length;
-//       let count = 0;
-
-//       if (diffLength > 0) {
-//         binaryCurNumber = "0" + binaryCurNumber;
-//       }
-
-//       for (let i = 0; i < binaryCurNumber.length; i++) {
-//         if (binaryCurNumber[i] !== binaryNextNumber[i]) {
-//           count++;
-//         }
-//       }
-
-//       if (count === 1 || count === 2) {
-//         result.push(parseInt(binaryNextNumber, 2));
-//         break;
-//       } else {
-//         nextNumber++;
-//       }
-//     }
-//   });
-// }
-
 function solution(numbers) {
   const result = [];
 
-  numbers.forEach((number) => {
-    let binaryCurNumber = number.toString(2);
+  for (const curNum of numbers) {
+    const curBinary = "0" + curNum.toString(2);
+    if (curNum % 2 === 0) {
+      result.push(curNum + 1);
+      continue;
+    }
 
-    if (number % 2 === 0) {
-      result.push(number + 1);
-    } else {
-      binaryCurNumber = "0" + binaryCurNumber;
+    for (let i = curBinary.length - 1; i >= 0; i--) {
+      if (curBinary[i] === "0") {
+        const convertedBinary =
+          curBinary.slice(0, i) + "10" + curBinary.slice(i + 2);
 
-      for (let i = binaryCurNumber.length - 1; i >= 0; i--) {
-        if (binaryCurNumber[i] === "0") {
-          result.push(
-            parseInt(
-              binaryCurNumber.substring(0, i) +
-                "10" +
-                binaryCurNumber.substring(i + 2, binaryCurNumber.length),
-              2
-            )
-          );
-          break;
-        }
+        result.push(parseInt(convertedBinary, 2));
+        break;
       }
     }
-  });
+  }
   return result;
 }
-
-solution([2, 7]);
