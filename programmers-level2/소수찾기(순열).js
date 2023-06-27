080735dcd4a263542bffe262869547d8fa7ca9b7
ssi02014 <ssi02014@naver.com>
2023-06-28 00:33:43 +0900

    refac: 소수찾기

--- programmers-level2/소수찾기(순열).js
+++ programmers-level2/소수찾기(순열).js
@@ -1,39 +1,73 @@
+// answer1
 function solution(numbers) {
-    let answer = 0;
-    let nums = new Set();
-    numbers = numbers.split("");
-    
-    makeNumber('', numbers);
-    
-    function isPrime(n) {
-        if (n < 2) {
-            return false;
-        }
-        for (let i = 2; i <= Math.sqrt(n); i++) {
-            if ( n % i === 0) {
-                return false;
-            }
-        }
-        return true;
+  const numberList = numbers.split("");
+  const numSet = new Set();
+  let answer = 0;
+
+  const getIsPrime = (n) => {
+    if (n < 2) return false;
+
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+      if (n % i === 0) {
+        return false;
+      }
     }
-    
-    function makeNumber(current, numbers) {
-        if (current.length > 0) {
-            
-            if (!nums.has(Number(current))) {
-                nums.add(Number(current));
-                
-                if (isPrime(Number(current))) {
-                    answer++;
-                }
-            }
+    return true;
+  };
+
+  const dfs = (current, numberList) => {
+    if (current.length) {
+      const isHasNum = numSet.has(+current);
+
+      if (!isHasNum) {
+        numSet.add(+current);
+
+        if (getIsPrime(+current)) {
+          answer++;
         }
-        for (let i = 0; i < numbers.length; i++) {
-            let tempArr = numbers.slice(0);
-            
-            tempArr.splice(i, 1);
-            makeNumber(current + numbers[i], tempArr);
-        }
+      }
     }
-    return answer;
+
+    for (let i = 0; i < numberList.length; i++) {
+      const tempArr = [...numberList];
+
+      tempArr.splice(i, 1);
+      dfs(current + numberList[i], tempArr);
+    }
+  };
+
+  dfs("", numberList);
+
+  return answer;
+}
+
+// answer2
+function solution(numbers) {
+  const visited = Array.from({ length: numbers.length }, () => false);
+  const numSet = new Set();
+  let result = 0;
+
+  const dfs = (target) => {
+    const isHasNum = numSet.has(+target);
+
+    if (!isHasNum) {
+      numSet.add(+target);
+
+      if (isPrimeNumber(+target)) {
+        result++;
+      }
+    }
+
+    for (let i = 0; i < numbers.length; i++) {
+      if (!visited[i]) {
+        visited[i] = true;
+        dfs(target + numbers[i]);
+        visited[i] = false;
+      }
+    }
+  };
+
+  dfs("");
+
+  return result;
 }
