708bac6fe2224f7682646578a100bb1550511603
ssi02014 <ssi02014@naver.com>
2023-07-08 13:40:23 +0900

    refactor: 삼각 달팽이

--- programmers-level2/삼각달팽이_구현.js
+++ programmers-level2/삼각달팽이_구현.js
@@ -1,23 +1,21 @@
 function solution(n) {
-  let numList = Array.from(Array(n), () => []);
+  const numList = Array.from({ length: n }, (_, idx) => Array(idx).fill(0));
   let row = -1;
   let col = 0;
   let num = 0;
 
-  numList = numList.map((el, idx) => {
-    for (let i = 0; i <= idx; i++) {
-      el.push(0);
-    }
-    return el;
-  });
-
   for (let i = n; i > 0; i -= 3) {
+    // to Bottom
     for (let j = 0; j < i; j++) {
       numList[++row][col] = ++num;
     }
+
+    // to Right
     for (let j = 0; j < i - 1; j++) {
       numList[row][++col] = ++num;
     }
+
+    // to Top
     for (let j = 0; j < i - 2; j++) {
       numList[--row][--col] = ++num;
     }
