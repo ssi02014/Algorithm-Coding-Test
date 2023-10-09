10b25eb18231455287e381681b1b566959426bfe
ssi02014 <ssi02014@naver.com>
2023-10-09 18:31:19 +0900

    add: 영어 끝말 잇기 풀이 추가

--- programmers-level2/영어끝말잇기(배열).js
+++ programmers-level2/영어끝말잇기(배열).js
@@ -1,25 +1,53 @@
+// solution1: stack
 function solution(n, words) {
-    const usedWords = [];
-    
-    let answer = [0, 0];
-    let [turn, player] = [0, 0];
-    let [beforeWord, currentWord] = ["", ""];
-    
-    usedWords.push(words[0]);
-    
-    for (let i = 1; i < words.length; i++) {
-        beforeWord = words[i-1];
-        currentWord = words[i];
-        
-        if (beforeWord[beforeWord.length - 1] == currentWord[0]
-            && !usedWords.includes(currentWord)) {
-            usedWords.push(currentWord);
-        } else {
-            turn = parseInt(i / n) + 1;
-            player = i % n + 1;
-            
-            return answer = [player, turn];
-        }
+  const stack = [words[0]];
+
+  for (let i = 1; i < words.length; i++) {
+    const curWord = words[i];
+    const lastWord = stack.at(-1);
+    const humanSeq = (i + 1) % n || n;
+    const turn = Math.ceil((i + 1) / n);
+    const isValid = !stack.includes(curWord) && curWord[0] === lastWord.at(-1);
+
+    if (!isValid) return [humanSeq, turn];
+    stack.push(curWord);
+  }
+
+  return [0, 0];
+}
+
+// solution2: object
+function solution(n, words) {
+  const countObj = {};
+  let humanSeq = 0;
+  let prevWord = "";
+
+  for (let i = 0; i < words.length; i += n) {
+    humanSeq++;
+
+    for (let j = i; j < i + n; j++) {
+      const curWord = words[j];
+
+      if (!prevWord) {
+        prevWord = curWord;
+        countObj[curWord] = 1;
+        continue;
+      }
+
+      const turn = (j % n) + 1;
+      const curWordFirstChar = curWord[0];
+      const prevWordLastChar = prevWord.at(-1);
+      const isEliminated =
+        curWordFirstChar !== prevWordLastChar || countObj[curWord] > 0;
+
+      if (isEliminated) {
+        return [turn, humanSeq];
+      }
+
+      countObj[curWord] = 1;
+      prevWord = curWord;
     }
-    return answer;
+  }
+
+  return [0, 0];
 }
