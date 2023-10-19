a02ccbf498f5e88f4b28cf252519427cfb3eb495
ssi02014 <ssi02014@naver.com>
2023-10-19 23:18:30 +0900

    refac: 연속 부분 수열의 합의 개수 리팩

--- programmers-level2/연속-부분-수열의-합의-개수.js
+++ programmers-level2/연속-부분-수열의-합의-개수.js
@@ -1,19 +1,15 @@
 function solution(elements) {
+  const set = new Set();
   const twoElements = [...elements, ...elements];
-  const sumSet = new Set();
 
-  for (let i = 0; i < elements.length; i++) {
-    let sliceLength = 1;
-
+  for (let i = 1; i <= elements.length; i++) {
     for (let j = 0; j < elements.length; j++) {
       const sum = twoElements
-        .slice(j, sliceLength + i)
+        .slice(j, i + j)
         .reduce((acc, cur) => acc + cur, 0);
 
-      sumSet.add(sum);
-      sliceLength++;
+      set.add(sum);
     }
   }
-
-  return sumSet.size;
+  return set.size;
 }
