6ac5ce6e83ce5e3e782880e5703502e2591a5f01
ssi02014 <ssi02014@naver.com>
2023-11-11 02:44:03 +0900

    add: 특정거리의도시찾기 bfs

---
+++ BaekJoon/DFS-BFS/18352-특정거리의도시찾기-bfs.js
@@ -0,0 +1,49 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const input = [];
+
+rl.on("line", function (line) {
+  input.push(line);
+}).on("close", function () {
+  const [n, m, k, x] = input[0].split(" ").map(Number);
+  const nodeList = input.slice(1).map((el) => el.split(" ").map(Number));
+  const graph = Array.from({ length: n + 1 }, () => []);
+  const visited = Array(n + 1).fill(false);
+  const result = [];
+
+  nodeList.forEach(([node1, node2]) => {
+    graph[node1].push(node2);
+  });
+
+  const bfs = (startNode) => {
+    const queue = [[startNode, 0]];
+
+    visited[startNode] = true;
+
+    while (queue.length) {
+      const [curNode, curDistance] = queue.shift();
+
+      if (curDistance === k) {
+        result.push(curNode);
+        continue;
+      }
+
+      for (const nextNode of graph[curNode]) {
+        if (!visited[nextNode]) {
+          visited[nextNode] = true;
+          queue.push([nextNode, curDistance + 1]);
+        }
+      }
+    }
+  };
+
+  bfs(x);
+
+  result.sort((a, b) => a - b);
+
+  console.log(result.length ? result.join("\n") : "-1");
+});
