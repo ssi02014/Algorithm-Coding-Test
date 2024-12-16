c09a44ea8c1e8be38ea822d28cd6b68ab1fba0f2
ssi02014 <ssi02014@naver.com>
2024-12-17 01:28:14 +0900

    fix: 기능 개발 2번째 풀이 추가

--- 코딩테스트합격자되기/section7-큐/기능개발.js
+++ 코딩테스트합격자되기/section7-큐/기능개발.js
@@ -23,3 +23,28 @@ function solution(progresses, speeds) {
   }
   return result;
 }
+
+/**
+ * 시간복잡도 O(N) 풀이
+ * 남은 일자를 계산해서 미리 구하고, 최대 남은 일자를 갱신하면서 풀이 접근방식
+ */
+function solution2(progresses, speeds) {
+  const leftDays = progresses.map((progress, idx) => {
+    return Math.ceil((100 - progress) / speeds[idx]);
+  });
+  const result = [];
+  let maxLeftDay = leftDays[0];
+  let count = 0;
+
+  for (let i = 0; i < progresses.length; i++) {
+    if (leftDays[i] <= maxLeftDay) {
+      count++;
+    } else {
+      result.push(count);
+      count = 1;
+      maxLeftDay = leftDays[i];
+    }
+  }
+  result.push(count); // 반복문 나오기 직전 count한 값은 result에 추가되지 않기 때문에 직접 한번 더 호출해야 함
+  return result;
+}
