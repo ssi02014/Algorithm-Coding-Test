30f6c9d75819e50295b8ae1dc2d4bfef5c625286
ssi02014 <ssi02014@naver.com>
2025-01-10 01:59:59 +0900

    오타 수정

--- leetCode/easy/350.Intersection_of_Two_Arrays.js
+++ leetCode/easy/350.Intersection_of_Two_Arrays.js
@@ -9,7 +9,7 @@
  */
 
 // 해당 문제는 교집합에서 중복 요소가 포함되어야 하기 때문에 투 포인터로 접근해야 됩니다.
-var intersection = function (nums1, nums2) {
+var intersect = function (nums1, nums2) {
   const result = [];
   const sortedNums1 = [...nums1].sort((a, b) => a - b);
   const sortedNums2 = [...nums2].sort((a, b) => a - b);
